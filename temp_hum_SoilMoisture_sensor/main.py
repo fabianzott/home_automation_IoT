2bfe42b5791f966bd8d81a9510145354f5ec90e0
Fabian L. Zott <85985274+fabianzott@users.noreply.github.com>
2023-10-14 11:39:06 +0200

    Update main.py
    Completely refactored main.py

--- temp_hum_SoilMoisture_sensor/main.py
+++ temp_hum_SoilMoisture_sensor/main.py
@@ -1,40 +1,47 @@
-import socket
-###### BME280 #########
 from machine import Pin, I2C
 from time import sleep
 import bme280
-from umqtt.robust import MQTTClient 
+from umqtt.robust import MQTTClient
+import machine
 
+def init_bme280():
+    i2c = I2C(scl=Pin(5), sda=Pin(4))
+    return bme280.BME280(i2c=i2c)
 
-# ESP8266 - Pin assignment
-i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
-bme = bme280.BME280(i2c=i2c)
+def init_mqtt():
+    server = "<IP_address>"
+    client_id = "temp_test"
+    client = MQTTClient(client_id, server, port=1883)
+    client.connect()
+    return client
 
-
-SERVER = '<IP_adress>'  # local MQTT Server Address -> Raspberry Pi
-CLIENT_ID = 'temp_test'
-TOPIC_temp = b'temperature/esp2866/'
-TOPIC_pres = b'pressure/esp2866/'
-TOPIC_hum = b'humidity/esp2866/'
-
-client = MQTTClient(CLIENT_ID, SERVER, port=1883)
-client.connect()
-
-
-
-while True:
+def read_sensor(bme):
     try:
-      ########## BME280 loop ###########
-      sensor_data = bme.values  
-      #print(bme.values)
-      temp, pres, hum = sensor_data
-      temp = float(temp[:-1])
-      pres = float(pres[:-3])
-      hum = float(hum[:-1])
-      #print(temp)
-      #print(pres)
-      #print(hum)
+        temp, pres, hum = bme.values
+        temp = float(temp[:-1])
+        hum = float(hum[:-1])
+        return temp, hum
+    except OSError:
+        print("Failed to read sensor")
+        return None, None
+
+def main():
+    bme = init_bme280()
+    client = init_mqtt()
+
+    while True:
+        temp, hum = read_sensor(bme)
         
+        if temp is not None and hum is not None:
+            msg_hum = f"{hum}%"
+            client.publish(b'humidity/esp2866/', msg_hum)
+        else:
+            print("Invalid sensor readings")
+
+        sleep(5)
+
+if __name__ == "__main__":
+    main()
       ################ MQTT #############     
       if isinstance(temp, float) and isinstance(hum, float):
           #msg_temp = str(temp) + "°C"
