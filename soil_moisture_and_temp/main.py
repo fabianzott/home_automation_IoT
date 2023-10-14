6745e7962fecde4fc91cd72403484ea65d78b719
Fabian L. Zott <85985274+fabianzott@users.noreply.github.com>
2023-10-14 11:48:55 +0200

    Update main.py
    refactored - clean code

--- soil_moisture_and_temp/main.py
+++ soil_moisture_and_temp/main.py
@@ -1,77 +1,54 @@
-# Complete project details at https://RandomNerdTutorials.com
-import socket
-###### BME280 #########
-from machine import Pin, I2C
-from machine import ADC
+from machine import Pin, I2C, ADC
 from time import sleep
 import bme280
-from umqtt.robust import MQTTClient # here umqtt.robust package is used
+from umqtt.robust import MQTTClient
+import machine
 
-######### Soil Moisture #####
-adc = ADC(0)
-max_val = 38143   # this value has to be observed and can change over time -> absolute calibration is too time consuming
-min_val = 11519   # this value has to be observed and can change over time -> absolute calibration is too time consuming
+def init_bme280():
+    i2c = I2C(scl=Pin(5), sda=Pin(4))
+    return bme280.BME280(i2c=i2c)
+
+def init_mqtt():
+    server = "<IP_address>"
+    client_id = "temp_test"
+    client = MQTTClient(client_id, server, port=1883)
+    client.connect()
+    return client
+
+def read_bme280(bme):
+    temp, pres, hum = bme.values
+    return float(temp[:-1]), float(pres[:-3]), float(hum[:-1])
+
+def read_moisture(adc, min_val, max_val):
+    moist = adc.read_u16()
+    return convert_to_percent(moist, min_val, max_val, 100, 0)
 
 def convert_to_percent(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return round((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
-# ESP8266 - Pin assignment
-i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
-bme = bme280.BME280(i2c=i2c)
+def main():
+    bme = init_bme280()
+    client = init_mqtt()
+    adc = ADC(0)
+    min_val, max_val = 11519, 38143
 
-
-SERVER = '<IP_adress>'  # local MQTT Server Address -> Raspberry Pi
-CLIENT_ID = 'temp_test'
-TOPIC_temp = b'temperature/esp2866/'
-TOPIC_pres = b'pressure/esp2866/'
-TOPIC_hum = b'humidity/esp2866/'
-TOPIC_moist = b'moisture/esp2866/'
-
-client = MQTTClient(CLIENT_ID, SERVER, port=1883)
-client.connect()
-
-
-
-while True:
-    try:
-      ########## BME280 loop ###########
-      sensor_data = bme.values  
-      #print(bme.values)
-      temp, pres, hum = sensor_data
-      temp = float(temp[:-1])
-      pres = float(pres[:-3])
-      hum = float(hum[:-1])
-      #print(temp)
-      #print(pres)
-      #print(hum)
-      
-      ########### Soil Moisture #########
-      moist = adc.read_u16()
-      moist = int(moist)
-      moist_percent =  convert_to_percent(moist, min_val, max_val, 100, 0) # sometimes neg -> dynamic calibration
-      moist_percent = round(moist_percent)
-      print(moist_percent)
+    while True:
+        temp, pres, hum = read_bme280(bme)
+        moist_percent = read_moisture(adc, min_val, max_val)
         
-      ################ MQTT #############     
-      if isinstance(temp, float) and isinstance(hum, float) and isinstance(pres, float) and isinstance(moist_percent, int):
-          msg_temp = str(temp)# + "°C"
-          msg_hum = str(hum)# + "%"
-          msg_pres = str(pres)
-          msg_moist = str(moist_percent)
-          #msg = (b'{0:3.2f}, {0:3.2f}'.format(temp, hum))
-          #.publish(TOPIC_temp, msg_temp)
-          client.publish(TOPIC_hum, msg_hum)
-          time.sleep_ms(100)
-          client.publish(TOPIC_temp, msg_temp)
-          time.sleep_ms(100)
-          client.publish(TOPIC_pres, msg_pres)
-          time.sleep_ms(100)
-          client.publish(TOPIC_moist, msg_moist)
-          time.sleep_ms(100)
-          #print(msg_temp, msg_hum)
-      else:
-          print('Invalid sensor readings')
-    except OSError:
-        print('Failed to read sensor')
+        if all(isinstance(val, (float, int)) for val in [temp, hum, pres, moist_percent]):
+            client.publish(b'humidity/esp2866/', f"{hum}")
+            sleep(0.1)
+            client.publish(b'temperature/esp2866/', f"{temp}")
+            sleep(0.1)
+            client.publish(b'pressure/esp2866/', f"{pres}")
+            sleep(0.1)
+            client.publish(b'moisture/esp2866/', f"{moist_percent}")
+            sleep(0.1)
+        else:
+            print("Invalid sensor readings")
 
-    sleep(10)
+        sleep(10)
+
+if __name__ == "__main__":
+    main()
